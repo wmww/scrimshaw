ad7c762804ffef364facb35a43ec72d4d80e282b
William Wold <wilcoze@gmail.com>
2018-04-09 21:27:33 -0400

    made it show on and off times

--- scripts/morse_gpio_input.py
+++ scripts/morse_gpio_input.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
 pin = 21
+
+def get_time():
+	return time.clock_gettime(time.CLOCK_MONOTONIC)
 
 try:
 	# use GPIO pin numbering
@@ -9,12 +12,18 @@ try:
 
 	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-	for i in range(50):
-		if GPIO.input(pin):
-			print('on')
-		else:
-			print('off')
-		sleep(0.1)
+	active_last = False;
+	time_last = get_time()
+	while True:
+		active = GPIO.input(pin)
+		if active_last != active:
+			active_last = active
+			time_current = get_time()
+			time_elapsed = time_current - time_last
+			time_last = time_current
+			state = 'on' if active else 'off'
+			print(state + ' for ' + str(time_elapsed * 1000.0) + 'ms')
+		time.sleep(0.005)
 
 finally:
 	GPIO.cleanup()
