2d47a6ac6d8ef67ddae2cf85376faac3a81232d2
William Wold <wilcoze@gmail.com>
2018-04-09 22:17:13 -0400

    interpret morse letters

--- scripts/morse_gpio_input.py
+++ scripts/morse_gpio_input.py
@@ -3,6 +3,37 @@ import time
 
 pin = 21
 
+letters_to_morse = {
+	'a': '.-',
+	'b': '-...',
+	'c': '-.-.',
+	'd': '-..',
+	'e': '.',
+	'f': '..-.',
+	'g': '--.',
+	'h': '....',
+	'i': '..',
+	'j': '.---',
+	'k': '-.-',
+	'l': '.-..',
+	'm': '--',
+	'n': '-.',
+	'o': '---',
+	'p': '.--.',
+	'q': '--.-',
+	'r': '.-.',
+	's': '...',
+	't': '-',
+	'u': '..-',
+	'v': '...-',
+	'w': '.--',
+	'x': '-..-',
+	'y': '-.--',
+	'z': '--..'
+}
+
+morse_to_letters = {j: i for i, j in letters_to_morse.items()}
+
 def get_time():
 	return time.clock_gettime(time.CLOCK_MONOTONIC)
 
@@ -14,31 +45,37 @@ try:
 
 	active_last = False;
 	time_last = get_time()
+	letter = ''
+	is_space = False
 	while True:
+		time_current = get_time()
+		time_elapsed = time_current - time_last
+		time_ms = time_elapsed * 1000.0
+		if letter != '' and time_ms > 500:
+			if letter in morse_to_letters:
+				print(morse_to_letters[letter], end='', flush=True)
+			else:
+				print(letter, end='', flush=True)
+			letter = ''
+			is_space = False
+		if not is_space and time_ms > 1500:
+			print(' ', end='', flush=True)
+			is_space = True
 		active = GPIO.input(pin)
 		if active_last != active:
 			active_last = active
-			time_current = get_time()
-			time_elapsed = time_current - time_last
 			time_last = time_current
-			state = 'on' if active else 'off'
-			time_ms = time_elapsed * 1000.0
 			if active:
 				if time_ms < 20:
 					pass # ignore noise
 				elif time_ms < 150:
-					print('.', end='', flush=True)
-				elif time_ms < 400:
-					print('-', end='', flush=True)
+					letter += '.'
+					#print('.', end='', flush=True)
 				else:
-					print('long (' + str(time_ms) + 'ms', end='', flush=True)
-			else:
-				if time_ms < 300:
-					pass # ignore short spaces
-				elif time_ms < 1500:
-					print(' ', end='', flush=True)
-				else:
-					print()
+					letter += '-'
+					#print('-', end='', flush=True)
+				#else:
+				#	print('long (' + str(time_ms) + 'ms', end='', flush=True)
 		time.sleep(0.005)
 
 finally:
