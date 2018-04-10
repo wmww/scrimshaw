e4a3fe6609f297e601fa3ab823201ff4932ee222
William Wold <wilcoze@gmail.com>
2018-04-09 21:48:44 -0400

    recognize dashes and dots

--- scripts/morse_gpio_input.py
+++ scripts/morse_gpio_input.py
@@ -22,7 +22,23 @@ try:
 			time_elapsed = time_current - time_last
 			time_last = time_current
 			state = 'on' if active else 'off'
-			print(state + ' for ' + str(time_elapsed * 1000.0) + 'ms')
+			time_ms = time_elapsed * 1000.0
+			if active:
+				if time_ms < 20:
+					pass # ignore noise
+				elif time_ms < 150:
+					print('.', end='', flush=True)
+				elif time_ms < 400:
+					print('-', end='', flush=True)
+				else:
+					print('long (' + str(time_ms) + 'ms', end='', flush=True)
+			else:
+				if time_ms < 300:
+					pass # ignore short spaces
+				elif time_ms < 1500:
+					print(' ', end='', flush=True)
+				else:
+					print()
 		time.sleep(0.005)
 
 finally:
