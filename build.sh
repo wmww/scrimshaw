344840fea4377f60064adc14a22b31eed2c10e8a
William Wold <william.wold@canonical.com>
2018-04-24 09:06:40 -0400

    switched from build script to CMake

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p build
-./clang-format-run.sh
-g++ -Wall -g -o build/bin -DDISABLE_WIRING_PI $(pkg-config --cflags gtk+-3.0) display/*.cpp display/epdif/*.cpp $(pkg-config --libs gtk+-3.0)
-exit $? # return if g++ worked
