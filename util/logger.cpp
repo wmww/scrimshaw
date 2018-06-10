b024dde067eaaedf25d1976fdf64d22943ce649e
William Wold <wm@wmww.sh>
2018-06-10 04:36:03 -0700

    added color to logger

--- util/logger.cpp
+++ util/logger.cpp
@@ -7,10 +7,21 @@ std::string log_level_to_string(LogLevel level)
 {
 	switch (level)
 	{
-	case LOG_MESSAGE: return "MESSAGE";
-	case LOG_WARNING: return "WARNING";
-	case LOG_FATAL: return "LOG_FATAL";
+	case LogLevel::message: return "MESSAGE";
+	case LogLevel::warning: return "WARNING";
+	case LogLevel::fatal: return "FATAL";
 	default: return "UNKNOWN LOG LEVEL";
+	}
+}
+
+std::string log_level_color_code(LogLevel level)
+{
+	switch (level)
+	{
+		case LogLevel::message: return "\033[31m";
+		case LogLevel::warning: return "\033[1;33m";
+		case LogLevel::fatal: return "\033[1;31m";
+		default: return "\033[0m";
 	}
 }
 
@@ -26,14 +37,20 @@ void log_internal(LogLevel level, std::string file_path, std::string func_name, 
 	std::lock_guard<std::mutex> lock(log_mutex);
 
 	auto stream = &std::cerr;
-	if (level == LOG_MESSAGE)
+	if (level == LogLevel::message)
 		stream = &std::cout;
 
-	if (level != LOG_MESSAGE)
-		*stream << log_level_to_string(level) << ": ";
+	if (level != LogLevel::message)
+		*stream << log_level_color_code(level) << log_level_to_string(level) << "\033[0m" << " ";
 
-	*stream << message << " [ " << file_path << ":" << line_num << " - " << func_name << "()"
-			<< " ] " << std::endl;
-	if (level == LOG_FATAL)
+	*stream << "\033[0;36m" << "[" << file_path << ":" << line_num;
+	*stream << (line_num < 1000 ? (line_num < 100 ? (line_num < 10 ? "   " : "  ") : " ") : "");
+	*stream << func_name << "]: " << "\033[0m";
+
+	*stream << message;
+
+	*stream << std::endl;
+
+	if (level == LogLevel::fatal)
 		exit(1);
 }
--- util/logger.h
+++ util/logger.h
@@ -1,22 +1,24 @@
 #pragma once
 #include "util/std.h"
 
-enum LogLevel { LOG_MESSAGE, LOG_WARNING, LOG_FATAL };
+enum class LogLevel { message, warning, fatal };
 
 void log_internal(LogLevel level, std::string file_path, std::string func_name, int line_num, std::string message);
 
-#define log_message(msg) log_internal(LOG_MESSAGE, __FILE__, __func__, __LINE__, msg);
-#define log_warning(msg) log_internal(LOG_WARNING, __FILE__, __func__, __LINE__, msg);
-#define log_fatal(msg) log_internal(LOG_FATAL, __FILE__, __func__, __LINE__, msg);
+#define log_message_off(msg)
+#define log_message(msg) log_internal(LogLevel::message, __FILE__, __func__, __LINE__, msg);
+#define log_warning(msg) log_internal(LogLevel::warning, __FILE__, __func__, __LINE__, msg);
+#define log_fatal(msg) log_internal(LogLevel::fatal, __FILE__, __func__, __LINE__, msg);
+#define assertion_text(condition) "assertion \033[40;37m" #condition "\033[0m failed"
 #define assert_else(condition, action)                    \
 	if (!(condition))                                     \
 	{                                                     \
-		log_warning("failed assertion '" #condition "'"); \
+		log_warning(assertion_text(condition)); \
 		action;                                           \
 	}
 #define assert_nonfatal(condition) assert_else(condition, )
 #define assert_fatal(condition)                         \
 	if (!(condition))                                   \
 	{                                                   \
-		log_fatal("failed assertion '" #condition "'"); \
+		log_fatal(assertion_text(condition)); \
 	}
